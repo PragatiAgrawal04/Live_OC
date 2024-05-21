011108d95ef5e2cd8dd23e1cc2158ee7fa504b2c
PragatiAgrawal04 <105303281+PragatiAgrawal04@users.noreply.github.com>
2024-05-21 12:07:13 +0530

    Update experiment.py

--- experiment.py
+++ experiment.py
@@ -294,8 +294,9 @@ def frag_table(table_number):
         col1, col2, col3 = st.columns(3)
 
         with col1:
-            output_ce = output_ce.style.set_properties(**{'background-color':'palegreen'})
-            st.dataframe(output_ce.style.format("{'lastPrice':.2%}"))
+            ce_form = output_ce.style.format("{:.2%}")
+            output_ce_disp = ce_form.style.set_properties(**{'background-color':'palegreen'})
+            st.dataframe(output_ce_disp)
         with col2:
             output_pe = output_pe.style.set_properties(**{'background-color': 'antiquewhite'})
             st.dataframe(output_pe)
