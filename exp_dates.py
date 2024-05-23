ef0ee6e056c449916d2f4e6a33d900a7777f66fd
PragatiAgrawal04 <105303281+PragatiAgrawal04@users.noreply.github.com>
2024-05-23 13:21:45 +0530

    Update exp_dates.py

--- exp_dates.py
+++ exp_dates.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 import datetime
 import streamlit as st
 import csv
-from mysql-connector-python import connection
+import mysql-connector-python
 
 
 st.set_page_config(page_title="Dashboard", layout="wide")
