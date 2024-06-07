11b9eb34d36d7dfe883533982b13c4c49ff9a7f7
PragatiAgrawal04 <105303281+PragatiAgrawal04@users.noreply.github.com>
2024-06-07 12:17:50 +0530

    Update experiment.py

--- experiment.py
+++ experiment.py
@@ -263,6 +263,7 @@ def highlight_ratio(val, column_name):
 
 @st.experimental_fragment
 def frag_table(table_number, selected_option='UBL', exp_option=EXP_OPTION):
+    st.write("---")
     shares = pd.read_csv("FNO Stocks - All FO Stocks List, Technical Analysis Scanner.csv")
     share_list = list(shares["Symbol"])
     selected_option = selected_option.strip()
@@ -445,7 +446,7 @@ def frag_table(table_number, selected_option='UBL', exp_option=EXP_OPTION):
             curr.to_csv('history.csv', mode='w', index=False, header=True)
         else:
             curr.to_csv('history.csv', mode='a', index=False, header=False)
-
+    st.write("---")
 
 hist = pd.read_csv("history.csv")
 hist_df = pd.DataFrame(hist)
